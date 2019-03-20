c369eaa8cb9315104c3e713ca3c7b2ee240f0ee4
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-03-20 16:42:17 -0300

    Thirteenth modification to .travis.yml

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,2 @@
-# frozen_string_literal: true
-
-# Class AplicationHelper
 module ApplicationHelper
 end
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,3 @@
-# frozen_string_literal: true
-
-# Class AplicationMailer
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
