f3dd9b11182a769eea791831e208992ffc83a6da
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-03-25 11:51:39 -0300

    Book Model #3

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ActiveRecord::Base
-    validates :genre, :author, :image, :title, :editor, :year, presence: true
+  validates :genre, :author, :image, :title, :editor, :year, presence: true
 end
