53ec15f6d4ce76bf5c29b32418e57a4f5d166f97
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-03-29 15:23:40 -0300

    Book_routes N1

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -1,4 +1,4 @@
-class ApplicationController < ActionController::Base
+class ApplicationsController < ActionController::Base
   include DeviseTokenAuth::Concerns::SetUserByToken
   protect_from_forgery with: :null_session
 end
