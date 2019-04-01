59821c66fb4bd3ffa830ca24ece40d5a72c29947
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-04-01 10:29:08 -0300

    Book_routes N4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,18 @@
 class BooksController < ApplicationController
   include Wor::Paginate
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: %i[show index]
   def show
-    @book = Book.find(params[:id])
+    book = Book.find(book_id)
+    render json: book
   end
 
   def index
     render_paginated Book.all
+  end
+
+  private
+
+  def book_id
+    params.require(:id)
   end
 end
