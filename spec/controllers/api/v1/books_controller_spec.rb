a094f6d9bed609fba31035f405a383868d998c0e
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-04-03 16:28:56 -0300

    Book_routes_tests N2

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -1,23 +1,24 @@
 require 'rails_helper'
-
-RSpec.shared_context 'when authenticated User' do
-  let(:user) { create(:user) }
-
-  before do
-    request.headers.merge! user.create_new_auth_token
-  end
-end
+require 'active_record/errors'
+require './spec/support/shared_context/authenticate_user.rb'
 
 describe BooksController, type: :controller do
-  include_context 'when authenticated User'
-
   describe 'GET #index' do
-    context 'when fetching all the books' do
+    context 'without authenticated user' do
+      subject!(:http_request) { get :index }
+
       let!(:books) { create_list(:book, 5) }
 
-      before do
-        get :index
+      it 'responses with 401 status' do
+        expect(response).to have_http_status(:unauthorized)
       end
+    end
+
+    context 'when fetching all the books' do
+      include_context 'with authenticated user'
+      subject!(:http_request) { get :index }
+
+      let!(:books) { create_list(:book, 5) }
 
       it 'responses with the books json' do
         expected = ActiveModel::Serializer::CollectionSerializer.new(
@@ -30,15 +31,29 @@ describe BooksController, type: :controller do
         expect(response).to have_http_status(:ok)
       end
     end
+
+    context 'without any book' do
+      include_context 'with authenticated user'
+      subject!(:http_request) { get :index }
+    end
   end
 
   describe 'GET #show' do
-    context 'when fetching a book' do
+    context 'without authenticated user' do
+      subject!(:http_request) { get :show, params: { id: book.id } }
+
       let!(:book) { create(:book) }
 
-      before do
-        get :show, params: { id: book.id }
+      it 'responses with 401 status' do
+        expect(response).to have_http_status(:unauthorized)
       end
+    end
+
+    context 'when fetching a book' do
+      include_context 'with authenticated user'
+      subject!(:http_request) { get :show, params: { id: book.id } }
+
+      let!(:book) { create(:book) }
 
       it 'responses with the book json' do
         expect(response.body).to eq BookSerializer.new(
@@ -48,6 +63,15 @@ describe BooksController, type: :controller do
 
       it 'responds with 200 status' do
         expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when fetching an unexisting book' do
+      include_context 'with authenticated user'
+      subject!(:http_request) { get :show, params: { id: -1 } }
+
+      it 'responses with 404 status: page not found' do
+        expect(response).to have_http_status(:not_found)
       end
     end
   end
