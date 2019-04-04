d2abf2c6346827877a1fc22b4edeb2b1af61c16b
ranuccimariano <mariano.ranucci@wolox.com.ar>
2019-04-04 13:18:46 -0300

    Book_routes_tests N3

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -9,7 +9,7 @@ describe BooksController, type: :controller do
 
       let!(:books) { create_list(:book, 5) }
 
-      it 'responses with 401 status' do
+      it 'responds with status 401 (UNAUTHORIZED)' do
         expect(response).to have_http_status(:unauthorized)
       end
     end
@@ -20,14 +20,14 @@ describe BooksController, type: :controller do
 
       let!(:books) { create_list(:book, 5) }
 
-      it 'responses with the books json' do
+      it 'responds with the books JSON' do
         expected = ActiveModel::Serializer::CollectionSerializer.new(
           books, each_serializer: BookSerializer
         ).to_json
         expect(response.body.to_json) =~ JSON.parse(expected)
       end
 
-      it 'responds with 200 status' do
+      it 'responds with status 200 (OK)' do
         expect(response).to have_http_status(:ok)
       end
     end
@@ -35,6 +35,14 @@ describe BooksController, type: :controller do
     context 'without any book' do
       include_context 'with authenticated user'
       subject!(:http_request) { get :index }
+
+      it 'responds with status 200 (OK)' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'responds with an empty body' do
+        expect(JSON.parse(response.body)['page']).to be_empty
+      end
     end
   end
 
@@ -44,7 +52,7 @@ describe BooksController, type: :controller do
 
       let!(:book) { create(:book) }
 
-      it 'responses with 401 status' do
+      it 'responds with status 401 (UNAUTHORIZED)' do
         expect(response).to have_http_status(:unauthorized)
       end
     end
@@ -55,13 +63,13 @@ describe BooksController, type: :controller do
 
       let!(:book) { create(:book) }
 
-      it 'responses with the book json' do
+      it 'resonds with the book json' do
         expect(response.body).to eq BookSerializer.new(
           book, root: false
         ).to_json
       end
 
-      it 'responds with 200 status' do
+      it 'responds with status 200 (OK)' do
         expect(response).to have_http_status(:ok)
       end
     end
@@ -70,7 +78,7 @@ describe BooksController, type: :controller do
       include_context 'with authenticated user'
       subject!(:http_request) { get :show, params: { id: -1 } }
 
-      it 'responses with 404 status: page not found' do
+      it 'responds with status 404 (PAGE NOT FOUND)' do
         expect(response).to have_http_status(:not_found)
       end
     end
